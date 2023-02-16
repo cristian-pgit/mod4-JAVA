1fa9e9e34549c9a8a45ddacccaf8f89461da6afe
Cristian <naitsircl@gmail.com>
2023-02-16 01:27:38 -0300

    individual 8

--- random/src/random/Individual8.java
+++ random/src/random/Individual8.java
@@ -1,7 +1,11 @@
 package random;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 
@@ -25,85 +29,87 @@ public class Individual8 {
 		System.out.println("Hola, Bienvenido al Sistema de funciones miscelaneas");
 	}
 	
-	//setea los asistentes preguntando nombre y luego nota X veces donde 
+	//pregunta nombre y luego nota X veces donde 
 		private static void ingresarAlumno_Nota() {
-			//definir array para nombre asistente y edades
+			//definir array para nombre alumno y nota
 			List<String> nombres = new ArrayList<String>();
 			List<Integer> notas = new ArrayList<Integer>();
 			//declaro contadores
-			int cont = 0;
-			String nombre = "n";
-			boolean salida = false;
+			boolean minimo = false;
+			String nombre = "";
+			String salida = "salir";
 			//se abre conexión a consola
 			Scanner pr = new Scanner(System.in);
 			//se inicia el blucle
-			while (!salida) {
+			while (!nombre.equalsIgnoreCase(salida) || !minimo) {
 				//generar bucle hasta que se  ingrese un nombre
 				boolean hayNombre = false;
 				while (!hayNombre) {
-					System.out.println("Ingrese el nombre del Alumno: ");
+					System.out.println("Ingrese el Nombre del Alumno: ");
 					nombre = pr.nextLine();
-					if(nombre.equals("salir")) {
-						salida = true;
-					} else if(nombre == null || nombre.equals("") || nombre.length()<3) {
-						System.out.println("Nombre no puede estar vacio");
-					} else { 
+					if (nombre.equalsIgnoreCase(salida)) {
+						hayNombre = true;
+						continue;
+					}
+					if (nombre == null || nombre.isEmpty()) {
+						System.out.println("Nombre no válido, ingese nuevamente");
+					} else {
 						hayNombre = true;
 						nombres.add(nombre);
 					}
 				}
-				//generar bucle hasta que se  ingrese edad
+				
+				if (nombre.equalsIgnoreCase(salida)) {
+					continue;
+				}
+				//generar bucle hasta que se ingrese edad
 				boolean hayNota = false;
 				while (!hayNota) {
 					System.out.println("Ingrese la Nota del Alumno: ");
 					String nota = pr.nextLine();
-					if (nota ==  null || nota.isEmpty() || !nota.matches("[0-9]+"))
+					if (nota.isEmpty() || !nota.matches("[0-9]+") || nota.length() !=2 
+							|| !nota.matches("^[1-6][0-9]|70$") )
 					{
 						System.out.println("Nota no válida");
-					} else if (Integer.parseInt(nota)< 10 && Integer.parseInt(nota)> 70) { 
-						System.out.println("Disfrute su jubilación");
 					} else  {
 						hayNota = true;
 						notas.add(Integer.parseInt(nota));
+						
 					}
 				}
-				cont++;
+				if (notas.size()>=2) {
+					minimo = true;
+				}
 			}
 			mostrarAlumnos(nombres, notas);
 		}
 		
 		//desplegar datos
 		private static void mostrarAlumnos(List<String> nombres, List<Integer> notas) {
-			
-			
-			for (int i=0; i <notas.size(); i++) {
-				int suma = notas.get(i);
-				int prom = suma/notas.size();
-				System.out.println("El Promedio del curso es: "+prom);
-			
-				
-			}
-	
-		}			
-//			int contMayor = 0;
-//			int contEntre = 0;
-//			int contMenor = 0;	
-//			for (int i = 0; i < nombres.size(); i++) {
-//				if (notas.get(i)>35){
-//					contMayor++;
-//					System.out.println("nombre: "+ nombres.get(i) + " - edad: " + notas.get(i));
-//				} else if (notas.get(i)>=25 && notas.get(i)<=35){
-//					contEntre++;
-//								System.out.println("nombre: "+ nombres.get(i) + " - edad: " + notas.get(i));
-//					} else  {
-//						contMenor++;
-//					System.out.println("nombre: "+ nombres.get(i) + " - edad: " + notas.get(i));
-//					}
-//			}
-//			System.out.println("Hay "+contMayor + " asistentes con edades mayores a 35");
-//			System.out.println("Hay "+contEntre + " asistentes con edades entre 25 a 35");
-//			System.out.println("Hay "+contMenor + " asistentes con edades menores a 25");
-			
+			int n = nombres.size();
+		    int suma = 0;
+		    
+		    // Crear mapa para asignar nombre y nota
+		    Map<String, Integer> map = new HashMap<>();
+		    for (int i = 0; i < n; i++) {
+		        map.put(nombres.get(i), notas.get(i));
+		        suma += notas.get(i);
+		    }
+		    
+		 // Calcular promedio
+		    double prom = (double) suma / n;
+		    System.out.println("El Promedio del curso es: " + prom);
+		    
+		 // Sortear entradas segun su valor
+		    List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
+		    Collections.sort(list, Map.Entry.comparingByValue());
+
+		 // obtener valores +alto y +bajo
+		    Map.Entry<String, Integer> masAlta = list.get(n - 1);
+		    Map.Entry<String, Integer> masBaja = list.get(0);
+		    System.out.println("La nota mas alta es: " + masAlta.getValue() + " de " + masAlta.getKey());
+		    System.out.println("La nota mas baja es: " + masBaja.getValue() + " de " + masBaja.getKey());
+		}
 		
 	
 	
